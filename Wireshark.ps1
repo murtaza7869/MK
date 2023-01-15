db574216040e77c883d03b14e0f9b15c5331bb3f
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-01-15 14:01:41 -0800

    Update Wireshark.ps1

--- Wireshark.ps1
+++ Wireshark.ps1
@@ -1,2 +1,2 @@
 Set-ExecutionPolicy Unrestricted -Force
-winget install wireshark -h --accept-package-agreements --force --scope machine
+Start-Process winget -ArgumentLis 'install wireshark -h --accept-package-agreements --force --scope machine'
