6abfe27b00ea0bd32a9808c66acce3b2386ca9c3
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-01-15 15:53:53 -0800

    Update Wireshark.ps1

--- Wireshark.ps1
+++ Wireshark.ps1
@@ -1,2 +1,2 @@
 Set-ExecutionPolicy Unrestricted -Force
-Start-Process "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_1.18.2691.0_x64__8wekyb3d8bbwe\winget.exe" -ArgumentList 'install wireshark -h --accept-package-agreements --force --scope machine'
+winget install -e -h --accept-source-agreements --accept-package-agreements --id WiresharkFoundation.Wireshark
