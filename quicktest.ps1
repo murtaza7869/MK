5e5d950258f5a52832581cf7c9560cdccdf7b8e5
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-02-08 13:32:22 -0800

    Create quicktest.ps1

---
+++ quicktest.ps1
@@ -0,0 +1,28 @@
+$file = Get-Item "C:\windows\System32\notepad.exe"
+$url = "http://dct.deepfreeze.com:4545/uploaddct"
+
+$boundary = [System.Guid]::NewGuid().ToString()
+$LF = "`r`n"
+$bytes = [System.Text.Encoding]::UTF8.GetBytes("--$boundary$LF")
+$bytes += [System.Text.Encoding]::UTF8.GetBytes("Content-Disposition: form-data; name=`"file`"; filename=`"$($file.Name)`"$LF")
+$bytes += [System.Text.Encoding]::UTF8.GetBytes("Content-Type: `"application/octet-stream`"$LF$LF")
+$bytes += [System.IO.File]::ReadAllBytes($file.FullName)
+$bytes += [System.Text.Encoding]::UTF8.GetBytes("$LF--$boundary--$LF")
+
+$request = [System.Net.WebRequest]::Create($url)
+$request.Method = "POST"
+$request.ContentType = "multipart/form-data; boundary=$boundary"
+$request.ContentLength = $bytes.Length
+$request.Timeout = [System.Int32]::MaxValue
+
+$requestStream = $request.GetRequestStream()
+$requestStream.Write($bytes, 0, $bytes.Length)
+$requestStream.Close()
+
+$response = $request.GetResponse()
+$responseStream = $response.GetResponseStream()
+$responseReader = new-object System.IO.StreamReader($responseStream)
+$responseText = $responseReader.ReadToEnd()
+$response.Close()
+
+Write-Output $responseText
