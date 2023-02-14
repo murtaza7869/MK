f874ea948aba43493e692a676536bb1fdfbe8f12
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-02-14 16:05:45 -0500

    Update MoveBackUP.ps1

--- MoveBackUP.ps1
+++ MoveBackUP.ps1
@@ -1,15 +1,1 @@
-$source = "O:\"
-md C:\MovedBackup
-$destination = "C:\MovedBackup"
-
-Get-ChildItem -Path $source -Recurse | ForEach-Object {
-    $dest = $_.FullName -Replace [regex]::Escape($source), $destination
-    if ($_.PSIsContainer) {
-        if (!(Test-Path -Path $dest)) {
-            New-Item -ItemType Directory -Path $dest
-        }
-    }
-    else {
-        Copy-Item -Path $_.FullName -Destination $dest
-    }
-}
+Move-Item -Path "O:\*" -Destination "C:\BackupThawSpace" -Recurse -Container
