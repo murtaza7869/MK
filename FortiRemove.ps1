1ae3f3b54a1e45b6eda97871b258c72a6a89ba76
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-02-14 10:27:43 -0500

    Update FortiRemove.ps1

--- FortiRemove.ps1
+++ FortiRemove.ps1
@@ -1,3 +1,1 @@
-set-executionpolicy -ExecutionPolicy Unrestricted -Scope LocalMachine
-Unblock-File -Path C:\FortiRemove.ps1
 Get-WmiObject Win32_Product -Filter "name like 'FortiClient'" | Select-Object -ExpandProperty IdentifyingNumber | ForEach-Object { & 'MSIEXEC.exe' '/x' $_}
