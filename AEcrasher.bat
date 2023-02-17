353aefc61615131ffd9965849fd96aa41a8b6a44
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-02-16 22:45:00 -0800

    Update AEcrasher.bat

--- AEcrasher.bat
+++ AEcrasher.bat
@@ -1,3 +1,5 @@
 Invoke-WebRequest 'https://github.com/murtaza7869/Deploy/raw/master/FRCClient.EXE' -OutFile 'c:\DelMe\FRCClient.EXE'
 Invoke-WebRequest 'https://github.com/murtaza7869/Deploy/raw/master/FRCClient.EXE' -OutFile 'c:\DelMe\FRCClient2.EXE'
 xcopy c:\Windows\System32\*.exe c:\DelMe /I
+dir c:\ > c:\delme\xyz.bat
+dir c:\ > c:\delme\abc.exe
