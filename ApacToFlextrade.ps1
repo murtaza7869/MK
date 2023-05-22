c550c3913bd256a3d54c414b3b37fb74f23793b0
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-05-22 15:57:15 -0700

    Create ApacToFlextrade.ps1

---
+++ ApacToFlextrade.ps1
@@ -0,0 +1,5 @@
+$url = "https://faronics-dfc-canada-production-installers.s3.ca-central-1.amazonaws.com/Custom/Evertz_UpdateServerUrlOnly.exe"
+$output = "C:\Windows\temp\SwitchSiteFlextradey.exe"
+$wc = new-object System.Net.WebClient
+$wc.DownloadFile($url, $output)
+Start-Process -FilePath "C:\Windows\temp\Evertz_UpdateServerUrlOnly.exe"
