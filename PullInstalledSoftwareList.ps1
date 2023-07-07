2b71acc5aa58dca0dc0051a6577959c088125e9d
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-07-07 15:38:49 -0700

    Update PullInstalledSoftwareList.ps1

--- PullInstalledSoftwareList.ps1
+++ PullInstalledSoftwareList.ps1
@@ -1,4 +1,3 @@
-md C:\applist
 Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize | Out-File -FilePath "C:\applist\InstalledProgram.log"
 cat "C:\applist\InstalledProgram.log"
 Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize | Out-File -FilePath "C:\applist\Wow6432_InstalledProgram.log"
