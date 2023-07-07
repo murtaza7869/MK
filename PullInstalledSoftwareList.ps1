be39641df9a3fcaf30f260926e9c263c109064e9
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-07-07 15:24:32 -0700

    Create PullInstalledSoftwareList.ps1
    queries the uninstall key and prints the output on screen

---
+++ PullInstalledSoftwareList.ps1
@@ -0,0 +1,4 @@
+Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize | Out-File -FilePath "C:\applist\InstalledProgram.log"
+cat "C:\applist\InstalledProgram.log"
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize | Out-File -FilePath "C:\applist\Wow6432_InstalledProgram.log"
+cat "C:\applist\Wow6432_InstalledProgram.log"
