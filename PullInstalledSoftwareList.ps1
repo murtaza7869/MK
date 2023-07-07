bf4cb54ce78936fc9e61fa45221d21ebbaaf300d
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-07-07 15:49:42 -0700

    Update PullInstalledSoftwareList.ps1

--- PullInstalledSoftwareList.ps1
+++ PullInstalledSoftwareList.ps1
@@ -1,3 +1,5 @@
+# This script queries the Uninstall reg key for installed software and prints in on scree.
+# Uncomment the Out-File line if you need it saved to a log file
 Get-ItemProperty HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\* |  Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
 #| Out-File -FilePath "C:\applist\InstalledProgram.log"
 # cat "C:\applist\InstalledProgram.log"
