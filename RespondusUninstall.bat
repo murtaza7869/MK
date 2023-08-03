877e54ad0a5653f4c0518471f3e031105b386891
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-08-03 13:21:19 -0700

    Update RespondusUninstall.bat

--- RespondusUninstall.bat
+++ RespondusUninstall.bat
@@ -1,6 +1,6 @@
 CD /D C:\Users\student\Downloads
 MD ldb2setup_2-0-8
-NET USE L: \\student-server\deepfreeze\ldb2setup_2-0-8
+NET USE L: "\\student-server\deepfreeze\ldb2setup_2-0-8" pima /user:localhost\student /persistent:no
 XCOPY L:\*.* C:\Users\student\Downloads\ldb2setup_2-0-8 /E /Y
 NET USE L: /delete 
 
