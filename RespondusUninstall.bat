beac88da0b427700d5ab7148763cd35128fcd321
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-08-03 09:40:23 -0700

    Update RespondusUninstall.bat

--- RespondusUninstall.bat
+++ RespondusUninstall.bat
@@ -1,9 +1,9 @@
-CD /D C:\Users\instructor\Downloads
+CD /D C:\Users\student\Downloads
 MD ldb2setup_2-0-8
 NET USE L: \\student-server\deepfreeze\ldb2setup_2-0-8
-XCOPY L:\*.* C:\Users\instructor\Downloads\ldb2setup_2-0-8 /E /Y
+XCOPY L:\*.* C:\Users\student\Downloads\ldb2setup_2-0-8 /E /Y
 NET USE L: /delete 
 
-CD /D C:\Users\instructor\Downloads\ldb2setup_2-0-8
+CD /D C:\Users\student\Downloads\ldb2setup_2-0-8
 
-setup /s /f1"C:\Users\instructor\Downloads\ldb2setup_2-0-8\unsetup.iss"
+setup /s /f1"C:\Users\student\Downloads\ldb2setup_2-0-8\unsetup.iss"
