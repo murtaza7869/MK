69eb53d5dcc466bb2dbe2f8fc3ca12c7c26874cb
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-11-28 10:54:26 -0800

    Update LogoffUsers.ps1

--- LogoffUsers.ps1
+++ LogoffUsers.ps1
@@ -3,5 +3,4 @@ $lgauurl = "https://github.com/murtaza7869/MK/raw/main/LogOffAllUsers.exe"
 $outputDir = "C:\Windows\Temp\"
 $logofferFilePath = $outputDir + "LogOffAllUsers.exe"
 Invoke-WebRequest -Uri $lgauurl -OutFile $logofferFilePath
-
 Start-Process -FilePath $logofferFilePath  -Wait
