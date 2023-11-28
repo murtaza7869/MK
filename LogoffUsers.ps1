3ebbb1d738282daa2661bfddd948d2588b4da448
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2023-11-28 10:54:03 -0800

    Update LogoffUsers.ps1

--- LogoffUsers.ps1
+++ LogoffUsers.ps1
@@ -1,5 +1,5 @@
 # *** TBD **** Add code to logoff all the logged in users
-$lgauurl = "https://dev.deepfreeze.com/Download/LogOffAllUsers.exe"
+$lgauurl = "https://github.com/murtaza7869/MK/raw/main/LogOffAllUsers.exe"
 $outputDir = "C:\Windows\Temp\"
 $logofferFilePath = $outputDir + "LogOffAllUsers.exe"
 Invoke-WebRequest -Uri $lgauurl -OutFile $logofferFilePath
