5e1cf66fc6c3fca2049f43dbfe1a30c06bcfd550
murtaza7869 <64930674+murtaza7869@users.noreply.github.com>
2024-06-13 13:59:50 -0700

    Update InstallTestMyPC.ps1

--- InstallTestMyPC.ps1
+++ InstallTestMyPC.ps1
@@ -1,6 +1,6 @@
 
 If (Test-Path -Path C:\Windows\temp\TBSMyPC_7.0.0.9.zip){
-Remove-Item 'C:\Windows\temp\PolarisClientsv74.zip' -Recurse | out-null
+Remove-Item 'C:\Windows\temp\TBSMyPC_7.0.0.9.zip' -Recurse | out-null
 }
 Invoke-WebRequest 'https://cuyahogalibrary.sharepoint.com/:u:/g/publicfolders/Eamn7u3eTDtPoJLSBXtcedgBi6wFdcgER8kZxI5V8YQ8kg?download=1' -OutFile 'C:\Windows\temp\TBSMyPC_7.0.0.9.zip'
 If (Test-Path -Path C:\ProgramData\Faronics\TBSMyPC ){
@@ -9,4 +9,4 @@ Remove-Item 'C:\ProgramData\Faronics\TBSMyPC' -Recurse | out-null
 md C:\ProgramData\Faronics\TBSMyPC
 Expand-Archive -LiteralPath C:\Windows\temp\TBSMyPC_7.0.0.9.zip -DestinationPath C:\ProgramData\Faronics\TBSMyPC
 ping localhost -n 9
-Start-Process -FilePath 'C:\ProgramData\Faronics\TBSMyPC\TBSMyPC_7.0.0.9\TBS_INSTALLER_7.0.0.9.exe'
+Start-Process -FilePath 'C:\ProgramData\Faronics\TBSMyPC\TBS_INSTALLER_7.0.0.9.exe'
